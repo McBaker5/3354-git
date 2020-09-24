d05fe49ef09fd56ec03f7a2eb7537c0985d65bb4
McBaker5 <jackson@bakerplace.org>
2020-09-24 11:33:34 -0500

    Update Main.java
    Updated the addArguments function to add an arbitrary amount of positive integers. Updated the error message in main to reflect this change.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,11 +7,15 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide multiple integers to add");
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+    	int ret = Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+    	for (int i = 2; i < args.length; i ++) {
+    		ret += Integer.valueOf(args[i]);
+    	}
+        return ret;
     }
 }
